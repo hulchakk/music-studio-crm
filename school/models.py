dcc9a82a458c1bdd8af8198426869b2b2f0f4360
Dmytro <ys33ys33ys55@gmail.com>
2026-02-20 16:37:42 +0100

    feat(school): add Teacher, Student and Room models

--- school/models.py
+++ school/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class Teacher(AbstractBaseUser):
+    telegram_id = models.CharField(max_length=55, null=True)
+    phone_number = models.CharField(max_length=12)
+    description = models.TextField(null=True)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    description = models.TextField(null=True)
+
+class Room(models.Model):
+    name = models.CharField(max_length=55)
+    description = models.TextField(null=True)
