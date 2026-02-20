f65ceb62fb41c07f009785a0edd4871481ec7b45
Dmytro <ys33ys33ys55@gmail.com>
2026-02-20 20:03:57 +0100

    feat(school): register all other models in admin

--- school/admin.py
+++ school/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from school.models import (
+    Lesson,
+    Room,
+    Student,
+    Subscription,
+    SubscriptionPlan,
     Teacher,
 )
 
@@ -40,4 +45,54 @@ class TeacherAdmin(UserAdmin):
                 }
             )
         ),
+    )
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "description",
+    )
+
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "lessons_count",
+        "price",
+    )
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+    )
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "teacher",
+        "plan",
+        "lessons_left",
+        "start_date",
+        "end_date",
+        "is_active",
+    )
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = (
+        "student",
+        "teacher",
+        "room",
+        "start_datetime",
+        "status",
     )
