cae7d4041cecc3af6b556e905f27f9e50f3b99ea
Dmytro <ys33ys33ys55@gmail.com>
2026-02-20 19:33:40 +0100

    fix(school): swap AbstractBaseUser for AbstractUser and reset migrations

--- school/models.py
+++ school/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 
-class Teacher(AbstractBaseUser):
+class Teacher(AbstractUser):
     telegram_id = models.CharField(max_length=55, null=True, unique=True)
     phone_number = models.CharField(max_length=12, unique=True)
     description = models.TextField(null=True)
