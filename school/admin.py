cde660e1244b0ccd6ba5cf949c58fc6c10466ac2
Dmytro <ys33ys33ys55@gmail.com>
2026-02-20 19:35:46 +0100

    feat(school): register Teacher model in admin

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from school.models import (
+    Teacher,
+)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + (
+        "telegram_id",
+        "phone_number",
+        "description",
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "telegram_id",
+                        "phone_number",
+                        "description",
+                    )
+                }
+            )
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "telegram_id",
+                        "phone_number",
+                        "description",
+                    )
+                }
+            )
+        ),
+    )
