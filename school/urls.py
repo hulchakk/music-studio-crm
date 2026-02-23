9d82d9b955e488ac7e766cd2bd3bc504903102be
Dmytro <ys33ys33ys55@gmail.com>
2026-02-23 13:15:31 +0100

    feat(school): implement home page using bootstrap

--- school/urls.py
+++ school/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from school.views import (
+    index,
+)
+
 
 urlpatterns = [
-     
+     path("", index, name="index"),
 ]
 
 app_name = "school"
--- school/views.py
+++ school/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from school.models import (
+    Teacher,
+    Student,
+    Lesson,
+)
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    teachers_count = Teacher.objects.count()
+    students_count = Student.objects.count()
+    lessons_count = Lesson.objects.count()
+    context = {
+        "teachers_count": teachers_count,
+        "students_count": students_count,
+        "lessons_count": lessons_count,
+    }
+
+    return render(request, "school/index.html", context=context)
