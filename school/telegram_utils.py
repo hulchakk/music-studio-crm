7dbe6ab64935990cacaa6cedaded1971fec0607c
Dmytro <ys33ys33ys55@gmail.com>
2026-03-04 14:21:55 +0100

    chore: move telegram bot token to environment variables

--- school/telegram_utils.py
+++ school/telegram_utils.py
@@ -1,9 +1,13 @@
 import requests
-from django.conf import settings
+import os
+from dotenv import load_dotenv, dotenv_values
+
+
+load_dotenv() 
 
 
 def send_telegram_msg(chat_id, text):
-    token = settings.TELEGRAM_BOT_TOKEN
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         "chat_id": chat_id,
