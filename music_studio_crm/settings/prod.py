2bdc371bc33ff35be734fa01a87637e5bbcf1ae4
Dmytro <ys33ys33ys55@gmail.com>
2026-03-05 13:49:36 +0100

    deploy: : add build.sh and Render host configuration

--- music_studio_crm/settings/prod.py
+++ music_studio_crm/settings/prod.py
@@ -4,9 +4,12 @@ from urllib.parse import urlparse, parse_qsl
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "*",
-]
+ALLOWED_HOSTS = []
+
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 STATIC_ROOT = "staticfiles/"
 
